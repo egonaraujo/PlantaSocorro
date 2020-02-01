9d4912ca952c5fb3f8bdb2cb369a80b99c3e62ba
Egon Araujo <dev.egon.araujo@gmail.com>
2020-02-01 15:07:03 -0200

    Tools and plants done, plus resolution of project
    Signed-off-by: Egon Araujo <dev.egon.araujo@gmail.com>

--- Plants/PlantController.gd
+++ Plants/PlantController.gd
@@ -4,7 +4,12 @@ export (float)var status1 = 10;
 export (float)var status2 = 10;
 export (float)var status3 = 10;
 
+signal plant_tapped
+signal plant_slash
+signal plant_hold_down
+signal plant_hold_up
 
+var isHolding
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,8 +20,37 @@ func _ready():
 #	pass
 
 func _input(event):
-	if (event is InputEventScreenTouch and event.is_pressed()):
+	if (event is InputEventScreenTouch):
+		if(event.is_pressed()):
+			var point = event.position
+			var space_state = get_world_2d().direct_space_state
+			var collidersTouched = space_state.intersect_point(point, 32, [],
+													 2147483647, false,true)
+			var touchPlant = false
+			for dict in collidersTouched:
+				if (dict.collider == $Sprite/Plant):
+					touchPlant = true
+					break
+			if(touchPlant):
+				print("Tap Plant")
+				isHolding = true
+				emit_signal("plant_hold_down")
+				emit_signal("plant_tapped")
+		if(!event.is_pressed() && isHolding):
+			isHolding=false
+			emit_signal("plant_hold_up")
+			var point = event.position
+			print("Tap up Plant")
+	if( event is InputEventScreenDrag):
 		var point = event.position
 		var space_state = get_world_2d().direct_space_state
-		print(space_state.intersect_point(point))
-		print (point)
+		var collidersTouched = space_state.intersect_point(point, 32, [],
+													 2147483647, false,true)
+		var slashedPlant = false
+		for dict in collidersTouched:
+			if (dict.collider == $Sprite/Leaf):
+				slashedPlant = true
+				break
+		if(slashedPlant && event.speed.length() > 700):
+			emit_signal("plant_slash")
+			print("Slash Plant")
---
+++ Tools/Tool.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+export (int)var toolID = -1;
+
+signal tool_selected
+
+func _input(event):
+	if (event is InputEventScreenTouch):
+		if(event.is_pressed()):
+			var point = event.position
+			var space_state = get_world_2d().direct_space_state
+			var collidersTouched = space_state.intersect_point(point, 32, [],
+													 2147483647, false,true)
+			var touchItem = false
+			for dict in collidersTouched:
+				if (dict.collider == $Area2D):
+					touchItem = true
+					break
+			if(touchItem):
+				print("Tap Item")
+				emit_signal("tool_selected",toolID)
